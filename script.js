3d19f0a723b6b19be81b3256f9a6ae43561abddc
Cathub04 <liusihong2004@gmail.com>
2022-04-01 17:58:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 alert(Date());
 function para{
-	getElementById('chge').innerHTML='If you see this it means it changed.'
+	getElementById('chge').innerHTML='If you see this it means it changed.';
 }
