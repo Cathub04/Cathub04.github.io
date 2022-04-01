7851fc97aaa97ec4417602ffcc31099f8d09a700
Cathub04 <liusihong2004@gmail.com>
2022-04-01 09:46:26 -0400

    Change file
    New Assignment

---
+++ EXJSJS.js
@@ -0,0 +1,8 @@
+function tst(){
+	document.getElementById('one').innerHTML="HIHIHI";
+}
+function sum(){
+	var a = parseFloat(prompt('Please enter the first number.'));
+	var b = parseFloat(prompt('Please enter the second number.'));
+	alert(a+b);
+}
